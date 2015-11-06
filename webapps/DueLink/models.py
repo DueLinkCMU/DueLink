86e4e31d6f0c42ba079e31f185b2777f18c318e2
Cyril-Engels <bbezchang@gmail.com>
2015-11-06 16:52:01 -0500

    copy models.py to project directory

--- webapps/DueLink/models.py
+++ webapps/DueLink/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_user')
+    nick_name = models.CharField(max_length=20)
+    school = models.ForeignKey(School)
+    profile_image = models.ImageField(upload_to='user_images', blank=True)
+    friends = models.ManyToManyField(User, related_name='profile_friends')
+
+    def __unicode__(self):
+        return self.nick_name
+
+
+class School(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Course(models.Model):
+    course_number = models.CharField(max_length=10)
+    course_name = models.CharField(max_length=50)
+    section = models.CharField(max_length=1)
+    school = models.ForeignKey(School)
+    instructor = models.CharField(max_length=40)
+    students = models.ManyToManyField(User, related_name='course_students')
+
+    def __unicode__(self):
+        return self.school + ": " + self.course_name
+
+
+class Deadline(models.Model):
+    name = models.CharField(max_length=20)
+    due = models.DateTimeField()
+    course = models.ForeignKey(Course)
+    students = models.ManyToManyField(User)
+
+    def __unicode__(self):
+        return self.course.school + ", " + self.course.course_name + ", "\
+               + self.name + " due on " + self.due
+
+
+class Task(models.Model):
+    deadline = models.ForeignKey(Deadline)
+    status = models.BooleanField()
+    description = models.CharField(max_length=100)
+    timestamp = models.DateTimeField()
+
