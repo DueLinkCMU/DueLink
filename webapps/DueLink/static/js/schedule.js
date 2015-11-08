d4c85aa36534394968bd789449e2dc553bc01903
prestonfff <fupimeng@gmail.com>
2015-11-08 17:15:21 -0500

    add some schedule staff

--- webapps/DueLink/static/js/schedule.js
+++ webapps/DueLink/static/js/schedule.js
@@ -1,67 +1,25 @@
 $(document).ready(function() {
-
+         console.log($.datepicker.formatDate('yyyy-mm-dd', new Date()));
 		$('#calendar').fullCalendar({
-			defaultDate: '2015-02-12',
+
+			defaultDate: $.datepicker.formatDate('yyyy-mm-dd', new Date()),
 			editable: true,
 			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2015-02-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2015-02-07',
-					end: '2015-02-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-02-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-02-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2015-02-11',
-					end: '2015-02-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-02-12T10:30:00',
-					end: '2015-02-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2015-02-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-02-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2015-02-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2015-02-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2015-02-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2015-02-28'
+			events: {
+                url: '/duelink/get_schedule/',
+                error: function() {
+					$('#script-warning').show();
 				}
-			]
+            },
+            loading: function(bool) {
+				$('#loading').toggle(bool);
+			}
 		});
 
 	});/**
  * Created by pimengfu on 11/7/15.
  */
+
+function get_schedule(){
+
+}
