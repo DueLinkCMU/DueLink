b5b7535b7f288dbfa14040e360b752501b6b0e23
prestonfff <fupimeng@gmail.com>
2015-11-08 21:35:20 -0500

    fix timezone problem

--- webapps/DueLink/static/js/schedule.js
+++ webapps/DueLink/static/js/schedule.js
@@ -1,24 +1,40 @@
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
+    get_schedule(function(events){
+        $('#calendar').fullCalendar({
 
         defaultDate: moment().format("YYYY-MM-DD"),
         editable: true,
         eventLimit: true, // allow "more" link when too many events
-        events: {
-        url: '/duelink/get_schedule/',
-            error: function() {
-            $('#script-warning').show();
-        }
-    },
+        events: events,
     loading: function(bool) {
         $('#loading').toggle(bool);
     }
+    });
 });
 
 });/**
  * Created by pimengfu on 11/7/15.
  */
 
-function get_schedule(){
+function get_schedule(callback){
+    $.get('/duelink/get_schedule/').done(function (data){
+         console.log("asd");
+        var events = data.events;
+        for (var i = 0; i < events.length; i++) {
+            events[i].start = convertUTCDateToLocalDate(events[i].start); //change data format
+        }
+        callback(events);
+    });
+}
 
+function convertUTCDateToLocalDate(str) {
+    //refer to http://stackoverflow.com/questions/10181649/convert-iso-timestamp-to-date-format-with-javascript
+    date = new Date(str);
+    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+
+    var offset = date.getTimezoneOffset() / 60;
+    var hours = date.getHours();
+
+    newDate.setHours(hours - offset);
+    return newDate.toISOString();
 }
