99f5546db904567b6420f1c696fd6de916a5c6f5
prestonfff <fupimeng@gmail.com>
2015-11-08 22:00:14 -0500

    add @property method schedule_json to model DueEvent

--- webapps/DueLink/static/js/schedule.js
+++ webapps/DueLink/static/js/schedule.js
@@ -2,15 +2,36 @@ $(document).ready(function() {
     get_schedule(function(events){
         $('#calendar').fullCalendar({
 
-        defaultDate: moment().format("YYYY-MM-DD"),
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-        events: events,
-    loading: function(bool) {
-        $('#loading').toggle(bool);
-    }
+            defaultDate: moment().format("YYYY-MM-DD"),
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: events
+        });
+
+
+        // CSRF set-up copied from Django docs
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        var csrftoken = getCookie('csrftoken');
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        });
     });
-});
 
 });/**
  * Created by pimengfu on 11/7/15.
@@ -18,7 +39,7 @@ $(document).ready(function() {
 
 function get_schedule(callback){
     $.get('/duelink/get_schedule/').done(function (data){
-         console.log("asd");
+        console.log("asd");
         var events = data.events;
         for (var i = 0; i < events.length; i++) {
             events[i].start = convertUTCDateToLocalDate(events[i].start); //change data format
